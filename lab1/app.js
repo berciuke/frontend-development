ad1af7b1f1749f7efda561c94300c9691b61128e
Arkadiusz Pająk <zwarek2@gmail.com>
2024-11-28 23:27:14 +0100

    pokemon details WIP

--- lab1/app.js
+++ lab1/app.js
@@ -8,6 +8,23 @@ const createPokemonElement = (i, data, imageSource) => {
   div.appendChild(imageElement);
   return div;
 };
+
+async function fetchDetails(pokemonID) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+  .then((res) => res.json())
+  .then((data) => {
+    const stat = data.stats.map((obj) => {
+      return {[obj["stat"]["name"]]: obj["base_stat"]}
+    })
+    console.log(stat);
+    
+    return
+    // return {
+    //   "types": data.types,
+
+    // }
+  })
+}
 fetch("https://pokeapi.co/api/v2/pokemon/")
   .then((res) => res.json())
   .then((res) => {
@@ -22,6 +39,8 @@ fetch("https://pokeapi.co/api/v2/pokemon/")
           const image = data.sprites.front_default;
           count++;
           list.appendChild(createPokemonElement(count, pokemon, image));
+          console.log(fetchDetails(data.id));
+            
         });
     });
   });
