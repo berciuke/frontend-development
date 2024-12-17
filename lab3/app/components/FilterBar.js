ce4208055c43c3ed766ef5b371b6a4e54d15ff2f
Arkadiusz Pająk <zwarek2@gmail.com>
2024-12-17 11:05:05 +0100

    lab3 filter buttons

--- lab3/app/components/FilterBar.js
+++ lab3/app/components/FilterBar.js
@@ -7,14 +7,17 @@ export default function FilterBar() {
   const router = useRouter();
   const searchParams = useSearchParams();
   console.log(searchParams);
-  
-  const [search, setSearch] = useState(searchParams.get("search") || "");
 
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+  const [type, setType] = useState(searchParams.get("type") || "");
+  const [limit, setLimit] = useState(searchParams.get('limit') || '200');
   useEffect(() => {
     const params = new URLSearchParams(searchParams.toString());
     if (search) params.set("search", search);
+    if (type) params.set('type', type);
+    if (limit) params.set('limit', limit);
     router.replace(`/pokemon?${params.toString()}`);
-  }, [search]);
+  }, [search, type, limit]);
 
   return (
     <div className="search-elements">
@@ -25,6 +28,23 @@ export default function FilterBar() {
         placeholder="Search for Pokémon"
         onChange={(e) => setSearch(e.target.value)}
       />
+      <label>
+        Type:
+        <select value={type} onChange={(e) => setType(e.target.value)}>
+          <option value="">All</option>
+          <option value="fire">Fire</option>
+          <option value="water">Water</option>
+          <option value="electric">Electric</option>
+        </select>
+      </label>
+      <label>
+        Limit:
+        <input
+          type="number"
+          value={limit}
+          onChange={(e) => setLimit(e.target.value)}
+        />
+      </label>
     </div>
   );
 }
